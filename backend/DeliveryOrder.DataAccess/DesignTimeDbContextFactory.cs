d4e64f4e555fee786a04f4ef7b235f759f720691
Artem Mikulskii <st107472@student.spbu.ru>
2024-09-09 22:08:47 +0300

    Add DbFactory to make migrations

---
+++ backend/DeliveryOrder.DataAccess/DesignTimeDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DeliveryOrder.DataAccess
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DeliveryOrderDbContext>
+    {
+        public DeliveryOrderDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DeliveryOrderDbContext>();
+            optionsBuilder.UseNpgsql("User ID=postgres;Password=password;Host=localhost;Port=5432;Database=deliverydb;");
+
+            return new DeliveryOrderDbContext(optionsBuilder.Options);
+        }
+    }
+}
